d25622422400535bb7c58c12b545311c3f929ea8
BrKenji <ninihiratafei@gmail.com>
2022-12-05 21:47:20 -0300

    Rearranged ROC Curve code to use y_pred values predicted using the model

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Tensorflow and Keras for creating neural network models
-from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, cohen_kappa_score, log_loss
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, cohen_kappa_score, roc_curve, auc, RocCurveDisplay
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 # import NN layers and others components
 import tensorflow as tf
@@ -17,6 +18,7 @@ from keras.layers import Dense
 from keras.utils import np_utils
 from keras.callbacks import EarlyStopping
 
+from itertools import cycle
 
 def main():
 
@@ -43,9 +45,12 @@ def main():
     encoded_diag = encoder.transform(y)
     # convert integers to dummy variables (i.e. one hot encoded)
     dummy_diag = np_utils.to_categorical(encoded_diag)
+    n_classes = dummy_diag.shape[1]
 
     # convert to numpy arrays
     X_soma = np.array(df_L10["SOMA"])
+
+    X_train, X_test, y_train, y_test = train_test_split(X_soma, dummy_diag)
     # ----------------------------------------------------------------------------------------------
     
     # Defining Model -------------------------------------------------------------------------------
@@ -105,19 +110,68 @@ def main():
     plt.show()
     # ----------------------------------------------------------------------------------------------
     # Evaluating the model - Confusio Matrix--------------------------------------------------------
-    preds = model.predict(X_soma)
+    preds = model.predict(X_test)
 
-    matrix = confusion_matrix(dummy_diag.argmax(axis=1), preds.argmax(axis=1))
+    matrix = confusion_matrix(y_test.argmax(axis=1), preds.argmax(axis=1))
     print(matrix)
 
     cm = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis] 
     for i in range(len(cm.diagonal())):
         print(f'{dict.get(i)} accuracy: {cm.diagonal()[i]}')
 
-    print(classification_report(dummy_diag.argmax(axis=1), preds.argmax(axis=1)))
+    print(classification_report(y_test.argmax(axis=1), preds.argmax(axis=1)))
 
-    print(f'AUROC score: {roc_auc_score(dummy_diag, preds, average="weighted", multi_class="ovr")}')
+    print(f'AUROC score: {roc_auc_score(y_test, preds, average="weighted", multi_class="ovr")}')
     # ----------------------------------------------------------------------------------------------
+
+    # ROC Evaluation Metric ------------------------------------------------------------------------
+    # Compute ROC curve and ROC area for each class
+    fpr = {}
+    tpr ={}
+    roc_auc = {}
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], preds[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])  
+
+    # Micro-average ROC Curve and ROC Area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), preds.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # Aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    # Interpolate all ROC Curves at these points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+    
+    # Avarage it and compute AUC
+    mean_tpr /= n_classes
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    plt.figure()
+    lw = 2
+
+    colors = cycle(["aqua", "darkorange", "cornflowerblue"])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(
+            fpr[i],
+            tpr[i],
+            color=color,
+            lw=lw,
+            label="ROC curve of class {0} (area = {1:0.2f})".format(dict.get(i), roc_auc[i]),
+        )
+
+    plt.plot([0, 1], [0, 1], "k--", lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Some extension of Receiver operating characteristic to multiclass with y_pred")
+    plt.legend(loc="lower right")
+    plt.show()
+    # ----------------------------------------------------------------------------------------------        
     print("Here Working")
 
 main()
